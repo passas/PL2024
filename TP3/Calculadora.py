16499fd2e77dfef82ef88adf2648ede874aadca8
Sérgio Costa <48985218+passas@users.noreply.github.com>
2024-02-24 00:42:33 +0000

    Calculadora

---
+++ TP3/Calculadora.py
@@ -0,0 +1,19 @@
+class Calculadora:
+
+	def __init__(self):
+		self._soma = 0.0
+		self.estado = False
+
+	def liga (self):
+		self.estado = True
+
+	def desliga (self):
+		self.estado = False
+		self._soma = 0.0
+
+	def soma (self, valor):
+		self._soma += valor
+
+	def get_soma (self):
+		if self.estado == True:
+			return self._soma
